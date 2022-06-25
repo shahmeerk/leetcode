251d032377436a7a40cf3e078c84191c58ec41de
shahmeer <shahmeer.a.khan@gmail.com>
2022-06-25 14:01:25 +0100

    Sum1D optimized

--- Sum1D.java
+++ Sum1D.java
@@ -1,11 +1,9 @@
 public class Sum1D {
     public static int[] runningSum(int[] nums) {
         int[] output = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            output[i] = 0;
-            for(int j = 0; j <= i; j++){
-                output[i] +=  nums[j];
-            }
+        output[0] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            output[i] = output[i-1] + nums[i];
         }
         return output;
     }
