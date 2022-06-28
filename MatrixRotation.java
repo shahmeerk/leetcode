c97e77ea5edb98d4deb18da3dc8f7f08504a61fc
shahmeer <shahmeer.a.khan@gmail.com>
2022-06-28 14:01:21 +0100

    Image (matrix) Rotation Solution

--- MatrixRotation.java
+++ MatrixRotation.java
@@ -4,6 +4,7 @@ public class MatrixRotation {
         int length = matrix.length;
         int temp;
 
+        //Transpose the matrix (only works for square matrices)
         for(int i = 0; i < length; i++){
             for(int j = i; j < length; j++){
                 temp = matrix[i][j];
@@ -12,9 +13,12 @@ public class MatrixRotation {
             }
         }
 
+        //Swap the first n/2 elements with their respective last elements
         for(int i =0; i < length; i++){
             for(int j = 0; j < length / 2; j++){
                 temp = matrix[i][j];
+
+                //swap i.e element [0][0] with element [0][2] (for 3x3 matrix)
                 matrix[i][j] = matrix[i][length - 1 - j];
                 matrix[i][length - 1 - j] = temp;
             }
