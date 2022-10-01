1f373703c0dcc1996d614311f1e725514ef7c7e0
shahmeer <shahmeer.a.khan@gmail.com>
2022-10-01 01:44:08 +0100

    RepeatingCharacters Solution, still needs more deeper understanding

---
+++ RepeatingCharacters.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class RepeatingCharacters {
+    public static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map= new HashMap<>();
+        int start=0, len=0;
+
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (map.get(c) >= start)
+                    start = map.get(c) + 1;
+            }
+            len = Math.max(len, i-start+1);
+            map.put(c, i);
+        }
+
+        return len;
+    }
+
+
+    public static void main(String args[]){
+        System.out.println(RepeatingCharacters.lengthOfLongestSubstring("dvdf"));
+    }
+}
