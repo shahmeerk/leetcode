9fea0b566ea43da9a0bfe4538f002e1626eef50c
Shahmeer Khan <shahmeer.a.khan@gmail.com>
2023-03-30 01:30:05 +0100

    -Max Profit

--- MaxProfit.java
+++ MaxProfit.java
@@ -1,13 +1,11 @@
 public class MaxProfit {
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        int currProfit = 0;
-
-        for(int i = 0; i < prices.length; i++){
-            currProfit = chooseDay(i, prices);
-            profit =  currProfit > profit ? currProfit : profit;
+        int buy=Integer.MAX_VALUE,sell=0;
+        for(int i=0;i<prices.length;i++){
+            buy=Math.min(buy,prices[i]);
+            sell=Math.max(sell,prices[i]-buy);
         }
-        return profit;
+        return sell;
     }
 
     public int chooseDay(int index, int[] prices){
